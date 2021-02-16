c2ffa20e352d43a098a1acb2f1f880075dc2f394
satyasimha.sriperambuduru <satyasimha.sriperambuduru@gmail.com>
2021-02-16 09:38:43 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/url", (req, res, next) => {
+    res.json(["Idly","Dosa","Wada","Puri"]);
+   });
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
